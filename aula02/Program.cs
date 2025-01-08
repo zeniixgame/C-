ec73fb18d6e2bebc0b495da2a7483699f4d99427
Nick <laribrito78@gmail.com>
2025-01-08 17:40:55 -0300

    Instalação programas
    instalando programas e criando arquivos executáveis

---
+++ aula02/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace aula02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zeniix Game");
+            Console.Write(args.GetValue(0));
+        }
+    }
+}
+
